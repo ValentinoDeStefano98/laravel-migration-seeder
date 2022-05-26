67b1c5f8185c5c589881443642869f1f68d536a1
ValentinoDeStefano98 <valentino.destefano46@gmail.com>
2022-05-26 18:01:06 +0200

    need to fix time check

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -11,7 +11,7 @@ class TrainController extends Controller
     //Rotta localhost/trains
     public function index(){
 
-        $trains = Train::all();
+        $trains = Train::where('departure_time(Y-m-d)', '==', 'NOW()');
 
         return view('pages.trains.index', compact('trains'));
     }
